432f6d6302e9fed0da1cd71f4bf1cb4dfc611f34
lalitm1004 <lalitm1004@gmail.com>
2025-11-23 17:02:36 +0530

    feat: create config reader

--- raft/src/main.rs
+++ raft/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use raft::{Cli, Config};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config = Config::from(Cli::parse());
+
+    println!("{:?}", config);
+
+    Ok(())
 }
